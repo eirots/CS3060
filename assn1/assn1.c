9b509b9b6e87cff9d2c8045292add422ee4ac1bc
eirots <adstorie07@gmail.com>
2025-01-13 14:31:25 -0700

    added some stuff

---
+++ assn1/assn1.c
@@ -0,0 +1,19 @@
+/*
+Andrew Storie
+CS 3060-X02 Spring 2025
+Assignment 1
+*/
+
+/* Promise of Originality 
+I promise that this source code file has, in its entirety, been written by myself and no other person or persons. 
+If at any time an exact copy of this source code is found to be used by another person in this term, I understand that both 
+myself and the studen that submitted the copy will receive a zero on this assignment.
+
+*/
+
+#include <stdio.h>
+
+int main(int argc, char *arv[]){
+	printf("Hello World\n");
+	return 0;
+}
